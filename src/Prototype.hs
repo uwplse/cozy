bfde5a5273ca5cfdd304df482ad2657e8e8ede5d
Calvin Loncaric <loncaric@cs.washington.edu>
2014-11-06 17:35:13 -0800

    Comments and cleanup

--- src/Prototype.hs
+++ src/Prototype.hs
@@ -13,22 +13,24 @@ data Comparison = Eq | Gt
 -- A query is sort of like a predicate that operates over
 -- one record with Fields and one set of QueryVars.
 data Query =
-    T |  -- match everything
+    T | -- match everything
     F | -- match nothing
     Cmp Field Comparison QueryVar |
     And Query Query
-    -- someday maybe we'll support Or too
+    -- someday maybe we'll support "Or" as well
     deriving (Eq, Show)
 
 -- Execution planning language
+-- A plan is a program that returns a collection of elements. "What kind of
+-- collection" is determined by the plan itself.
 data Plan =
     All |
     None |
-    HashLookup Field QueryVar |
-    BinarySearch Field Comparison QueryVar |
-    Filter Plan Query |
-    SubPlan Plan Plan |
-    Intersect Plan Plan
+    HashLookup Field QueryVar | -- get a singleton collection matching the predicate
+    BinarySearch Field Comparison QueryVar | -- get a sorted list of elements
+    Filter Plan Query | -- execute the plan, then filter the result according to the predicate
+    SubPlan Plan Plan | -- super-tricky: execute the left plan, then execute the right plan on the result
+    Intersect Plan Plan -- execute the left plan and right plan, keeping only elements from both result sets
     deriving (Show)
 
 -- Data structures
@@ -47,8 +49,7 @@ data Cost =
     Factor Rational |
     Log Cost |
     Times Cost Cost |
-    Plus Cost Cost |
-    Max Cost Cost
+    Plus Cost Cost
     deriving (Show)
 
 -- For a given plan, extract the data structure necessary to execute it.
@@ -62,7 +63,7 @@ structureFor p = helper p Ty
         mkPoly Ty = UnsortedList Ty
         mkPoly x = x
 
-        helper All                    base = base
+        helper All                    base = mkPoly base
         helper None                   base = Empty
         helper (HashLookup f v)       base = HashMap f (mkPoly base)
         helper (BinarySearch f cmp v) base = SortedList f base
@@ -94,7 +95,7 @@ answersQuery p q = postCond p `implies` q
         postCond (Intersect p1 p2) = And (postCond p1) (postCond p2)
 
 -- Estimate how long a plan takes to execute.
--- Our overall goal is to find a plan that minimizes this.
+-- Our overall goal is to find a plan that minimizes this for large N.
 cost :: Plan -> Cost
 cost p = helper p N
     where
@@ -103,10 +104,17 @@ cost p = helper p N
         helper (HashLookup _ _) base = Factor 1
         helper (BinarySearch _ _ _) base = Log base
         helper (Filter p _) base = helper p base
+        -- The 0.5 in the next line needs some justification...
+        -- Basically, in the absence of any other info, we assume that the
+        -- probability of an object matching a given predicate is 50%, so
+        -- the subplan will execute on (we expect) half the data.
         helper (SubPlan p1 p2) base = Plus (helper p1 base) (helper p2 (Times base (Factor 0.5)))
-        helper (Intersect p1 p2) base = Max (helper p1 base) (helper p2 base)
+        helper (Intersect p1 p2) base = Plus (helper p1 base) (helper p2 base)
 
+-- A query. Sort of like "SELECT * WHERE age > x AND name = y" where x and y can vary.
 query = And (Cmp "age" Gt "x") (Cmp "name" Eq "y")
+
+-- Various candidate plans to implement said query.
 plan1 = Intersect (BinarySearch "age" Gt "x") (HashLookup "name" "y")
 plan2 = SubPlan (HashLookup "name" "y") (BinarySearch "age" Gt "x")
 plan3 = Filter All query
@@ -114,20 +122,11 @@ plan4 = Filter All (Cmp "name" Eq "y")
 
 main :: IO ()
 main = do
-    putStrLn $ "Query:    " ++ show query
-    putStrLn $ "Plan 1:   " ++ show plan1
-    putStrLn $ "    -->   structure -> " ++ show (structureFor plan1)
-    putStrLn $ "    -->   valid? " ++ show (plan1 `answersQuery` query)
-    putStrLn $ "    -->   cost = " ++ show (cost plan1)
-    putStrLn $ "Plan 2:   " ++ show plan2
-    putStrLn $ "    -->   structure -> " ++ show (structureFor plan2)
-    putStrLn $ "    -->   valid? " ++ show (plan2 `answersQuery` query)
-    putStrLn $ "    -->   cost = " ++ show (cost plan2)
-    putStrLn $ "Plan 3:   " ++ show plan3
-    putStrLn $ "    -->   structure -> " ++ show (structureFor plan3)
-    putStrLn $ "    -->   valid? " ++ show (plan3 `answersQuery` query)
-    putStrLn $ "    -->   cost = " ++ show (cost plan3)
-    putStrLn $ "Plan 4:   " ++ show plan4
-    putStrLn $ "    -->   structure -> " ++ show (structureFor plan4)
-    putStrLn $ "    -->   valid? " ++ show (plan4 `answersQuery` query)
-    putStrLn $ "    -->   cost = " ++ show (cost plan4)
+    putStrLn $ "Query: " ++ show query
+    mapM_ printPlanInfo [plan1, plan2, plan3, plan4]
+    where
+        printPlanInfo p = do
+            print p
+            putStrLn $ "    -->   structure = " ++ show (structureFor p)
+            putStrLn $ "    -->   valid? " ++ show (p `answersQuery` query)
+            putStrLn $ "    -->   cost = " ++ show (cost p)
