46f229502e3672c210a3f93c75df9bae279f3669
Calvin Loncaric <c.loncaric@partner.samsung.com>
2015-08-26 12:15:01 -0700

    Added check.sh script

---
+++ check.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+EXIT=0
+
+function err {
+    echo -e "\033[1;31m$1\033[0;0m"
+    EXIT=1
+}
+
+function require-program {
+    echo -n "checking for $1... "
+    if ! which $1; then
+        err "missing!"
+        return 1
+    fi
+}
+
+function check-python-version {
+    echo -n "checking for python 2.7... "
+    VERSION="$(python -c 'import sys; print(sys.version); sys.exit(sys.version_info < (2,7) or sys.version_info >= (2,8))' | head -n1)"
+    if [[ $? != 0 ]]; then
+        err "found $VERSION"
+        return 1
+    else
+        echo "$VERSION"
+    fi
+}
+
+function check-z3 {
+    echo -n "checking for z3 python module... "
+    if ! python -c "import z3" >/dev/null 2>&1; then
+        err "not found"
+        return 1
+    else
+        echo "found"
+    fi
+}
+
+function check-z3-version {
+    echo -n "checking for z3 version >= 4.4... "
+    VERSION="$(python -c 'import z3,sys; print(z3.get_version_string()); sys.exit(z3.get_version() < (4,4))')"
+    if [[ $? != 0 ]]; then
+        err "found $VERSION"
+        return 1
+    else
+        echo "$VERSION"
+    fi
+}
+
+require-program python && (check-python-version; (check-z3 && check-z3-version))
+
+if [[ $EXIT == 0 ]]; then
+    echo "SUCCESS!"
+fi
+
+exit $EXIT
