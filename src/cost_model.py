b43bf7007f556c3cd373cffff48cdfdece37c872
Calvin Loncaric <loncaric@cs.washington.edu>
2016-02-27 13:34:24 -0800

    Fixed broken cost model
    The cost model no longer has the potential to asymptotically approach
    some value. This allows us to fix the termination argument.
    
    I have confirmed that this does not affect Cozy's generated structure on
    any of the case studies.

--- src/cost_model.py
+++ src/cost_model.py
@@ -15,18 +15,18 @@ def _cost(plan, n=float(1000)):
         return cost1 + 1, size1 / 3
     if isinstance(plan, plans.BinarySearch):
         cost1, size1 = _cost(plan.plan)
-        return cost1 + (math.log(size1) if size1 >= 1 else 1), size1 / 2
+        return cost1 + (math.log(size1) if size1 >= 1 else 1) + 1, size1 / 2
     if isinstance(plan, plans.Filter):
         cost1, size1 = _cost(plan.plan)
-        return cost1 + size1, size1 / 2
+        return cost1 + size1 + 1, size1 / 2
     if isinstance(plan, plans.Intersect):
         cost1, size1 = _cost(plan.plan1)
         cost2, size2 = _cost(plan.plan2)
-        return cost1 + cost2 + size1 + size2, min(size1, size2) / 2
+        return cost1 + cost2 + size1 + size2 + 1, min(size1, size2) / 2
     if isinstance(plan, plans.Union):
         cost1, size1 = _cost(plan.plan1)
         cost2, size2 = _cost(plan.plan2)
-        return cost1 + cost2 + size1 + size2, size1 + size2
+        return cost1 + cost2 + size1 + size2 + 1, size1 + size2
     if isinstance(plan, plans.Concat):
         cost1, size1 = _cost(plan.plan1)
         cost2, size2 = _cost(plan.plan2)
