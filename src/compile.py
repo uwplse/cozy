0e45e45d8e6c3adb5af971687bae2b387642c473
Haoming Liu <haoming9502@gmail.com>
2016-04-21 23:46:43 -0700

    basic compile done including a couple of hacks

--- src/compile.py
+++ src/compile.py
@@ -6,41 +6,62 @@ class JavaPrinter(common.Visitor):
         s = "public class {} {{\n".format(spec.name)
         for name, t in spec.types:
             if type(t) == TEnum:
-               s += "public static enum {} {{\n{}\n}}".format(name, ",".join(t.cases))
-            # s += "  type {} = {}\n".format(name, self.visit(t))
-        # for name, t in spec.statevars:
-        #     s += "  state {} : {}\n".format(name, self.visit(t))
-        # for e in spec.assumptions:
-        #     s += "  assume {};\n".format(self.visit(e))
-        # for op in spec.methods:
-        #     s += str(self.visit(op))
+                s += self.visit(t, name, "create")
+            elif type(t) == TApp:
+                s += self.visit(t, name, "create")
+            else:
+                s += "{} {}\n".format(self.visit(t), name)
+        for name, t in spec.statevars:
+            s += "  {} {};\n".format(self.visit(t), name)
+        for e in spec.assumptions:
+            s += "putlic static boolean assumption {{\n {}\n }}\n\n".format(self.visit(e))
+        for op in spec.methods:
+            s += str(self.visit(op))
         s += "}"
         return s
 
-    def visit_TEnum(self, enum):
-        return "{{ {} }}".format(", ".join(enum.cases))
+    def visit_THandle(self, handle=None):
+        return "Entry" # hack
+
+    def visit_TEnum(self, enum, name=None, option=None):
+        if option == "create":
+            return "putlic static enum {} {{\n {} \n}} \n".format(name, ",".join(enum.cases))
+        else:
+            print "unknown option"
 
     def visit_TNamed(self, named):
         return named.id
 
-    def visit_TApp(self, app):
-        return "{}<{}>".format(app.t, self.visit(app.args))
+    def visit_TApp(self, app, name=None, option=None):
+        if option == "create":
+            return "public class {} {{\n {} \n}}\n".format(name, self.visit(app.args))
+        elif option == None:
+            return "{}<{}>".format(app.t, self.visit(app.args))
+        else:
+            print "unknow option"
 
     def visit_TRecord(self, r):
         return "{{ {} }}".format(", ".join("{} : {}".format(f, self.visit(t)) for f, t in r.fields))
 
+    def visit_TList(self, l):
+        return "List<{}>".format(self.visit(l.t))
+
+    def visit_TInt(self, i):
+        return "Integer"
+
     def visit_Query(self, q):
-        s = "  query {}({}):\n".format(q.name, ", ".join("{} : {}".format(name, self.visit(t)) for name, t in q.args))
-        for e in q.assumptions:
-            s += "    assume {};\n".format(self.visit(e))
-        s += "    {}\n".format(self.visit(q.ret))
-        return s
+        ret_type = self.visit(q.ret.type)
+    	s = "public {} {} ({}) {{\n {} result;\n {} \n}}\n\n".format(
+            ret_type, 
+            q.name,
+            ", ".join("{} {}".format(self.visit(t), name) for name, t in q.args), 
+            ret_type,
+            self.visit(q.ret))
+    	return s
 
     def visit_Op(self, q):
-        s = "  op {}({}):\n".format(q.name, ", ".join("{} : {}".format(name, self.visit(t)) for name, t in q.args))
-        for e in q.assumptions:
-            s += "    assume {};\n".format(self.visit(e))
-        s += "    {}\n".format(self.visit(q.body))
+        s = "public void {} ({}) {{\n {} \n}}\n\n".format(q.name, ", ".join("{} {}".format(self.visit(t), name) for name, t in q.args), 
+            self.visit(q.body))
         return s
 
     def visit_EVar(self, e):
@@ -56,16 +77,41 @@ class JavaPrinter(common.Visitor):
         return "({} {} {})".format(self.visit(e.e1), e.op, self.visit(e.e2))
 
     def visit_EUnaryOp(self, e):
-        return "({} {})".format(e.op, self.visit(e.e))
+        s = self.visit(e.e, e.op)
+        return s
 
     def visit_EGetField(self, e):
-        return "({}).{}".format(self.visit(e.e), e.f)
+        s = "({}).{}".format(self.visit(e.e), e.f)
+        return s
 
     def visit_EMakeRecord(self, e):
         return "{{ {} }}".format(", ".join("{} : {}".format(name, val) for name, val in e.fields))
 
-    def visit_EListComprehension(self, e):
-        return "[{} | {}]".format(self.visit(e.e), ", ".join(self.visit(clause) for clause in e.clauses))
+    def visit_EListComprehension(self, e, op=None):
+        s = ""
+        cpull_clause = ""
+        ccond_clause = []
+        for clause in e.clauses:
+            if type(clause) == CPull:
+                cpull_clause += self.visit(clause)
+            elif type(clause) == CCond:
+                ccond_clause.append(self.visit(clause))
+        s += cpull_clause + "{\n"
+        for clause in ccond_clause:
+            s += (clause + "{\n")
+
+        if op == None:
+            s += "{}.add(e);\n".format("result")
+        elif op == "sum":
+            s += "{} += {};\n".format("result", self.visit(e.e))
+        elif op == "some":
+            s += "{} = {};\n".format("result", self.visit(e.e))
+        elif op == "unique":
+            s += "assert unique({});\n".format(self.visit(e.e))
+        for clause in ccond_clause:
+            s += "}\n"
+        s += "}\n"
+        return s
 
     def visit_EAlloc(self, e):
         return "new {}({})".format(self.visit(e.t), ", ".join(self.visit(arg) for arg in e.args))
@@ -74,10 +120,10 @@ class JavaPrinter(common.Visitor):
         return "{}({})".format(e.func, ", ".join(self.visit(arg) for arg in e.args))
 
     def visit_CPull(self, c):
-        return "{} <- {}".format(c.id, self.visit(c.e))
+        return "for ({} {}: {})".format(self.visit_THandle(), c.id, self.visit(c.e))
 
     def visit_CCond(self, c):
-        return self.visit(c.e)
+        return "if ({})".format(self.visit(c.e))
 
     def visit_SNoOp(self, s):
         return "()"
@@ -86,7 +132,12 @@ class JavaPrinter(common.Visitor):
         return "{}.{}({})".format(s.target, s.func, ", ".join(self.visit(arg) for arg in s.args))
 
     def visit_SAssign(self, s):
-        return "{}.{} = {}".format(s.target, s.field, self.visit(s.rhs))
+        return "{}.{} = {}".format(self.visit(s.lhs), s.lhs.f, self.visit(s.rhs))
 
     def visit_SDel(self, s):
         return "del {}".format(self.visit(s.e))
+
+
+
+
+
