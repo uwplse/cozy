53f407c627f28b8907a32b08169b288bf1b46b9d
Calvin Loncaric <loncaric@cs.washington.edu>
2016-05-18 13:18:13 -0700

    Cleanup: moved declare_case to common.py

--- src/common.py
+++ src/common.py
@@ -77,3 +77,25 @@ def memoize(f):
         return result
 
     return wrapper
+
+def declare_case(supertype, name, attrs=()):
+    """
+    Usage:
+        CaseName = declare_case(SuperType, "CaseName", ["member1", ...])
+
+    Creates a new class (CaseName) that is a subclass of SuperType and has all
+    the given members.
+    """
+    class T(supertype):
+        def __init__(self, *args):
+            assert len(args) == len(attrs), "{} expects {} args, was given {}".format(name, len(attrs), len(args))
+            for attr, val in zip(attrs, args):
+                setattr(self, attr, val)
+        def __str__(self):
+            return repr(self)
+        def __repr__(self):
+            return "{}({})".format(name, ", ".join("{}={}".format(attr, repr(val)) for attr, val in zip(attrs, self.children())))
+        def children(self):
+            return tuple(getattr(self, a) for a in attrs)
+    T.__name__ = name
+    return T
--- src/syntax.py
+++ src/syntax.py
@@ -2,69 +2,48 @@
 AST definitions.
 """
 
-from common import ADT
+from common import ADT, declare_case
 
-def declare_case(supertype, name, attrs=()):
-    """
-    Usage: declare_case(SuperType, "CaseName", ["member1", ...])
-
-    Creates a new class (CaseName) that is a subclass of SuperType and has all
-    the given members.
-    """
-    class T(supertype):
-        def __init__(self, *args):
-            assert len(args) == len(attrs), "{} expects {} args, was given {}".format(name, len(attrs), len(args))
-            for attr, val in zip(attrs, args):
-                setattr(self, attr, val)
-        def __str__(self):
-            return repr(self)
-        def __repr__(self):
-            return "{}({})".format(name, ", ".join("{}={}".format(attr, repr(val)) for attr, val in zip(attrs, self.children())))
-        def children(self):
-            return tuple(getattr(self, a) for a in attrs)
-    T.__name__ = name
-    globals()[name] = T
-
-declare_case(ADT, "Spec", ["name", "types", "statevars", "assumptions", "methods"])
+Spec                = declare_case(ADT, "Spec", ["name", "types", "statevars", "assumptions", "methods"])
 
 class Method(ADT): pass
-declare_case(Method, "Op",    ["name", "args", "assumptions", "body"])
-declare_case(Method, "Query", ["name", "args", "assumptions", "ret"])
+Op                  = declare_case(Method, "Op",    ["name", "args", "assumptions", "body"])
+Query               = declare_case(Method, "Query", ["name", "args", "assumptions", "ret"])
 
 class Type(ADT): pass
-declare_case(Type, "TInt")
-declare_case(Type, "TLong")
-declare_case(Type, "TBool")
-declare_case(Type, "THandle", ["t"])
-declare_case(Type, "TBag",    ["t"])
-declare_case(Type, "TList",   ["t"])
-declare_case(Type, "TSet",    ["t"])
-declare_case(Type, "TMap",    ["k", "v"])
-declare_case(Type, "TNamed",  ["id"])
-declare_case(Type, "TRecord", ["fields"])
-declare_case(Type, "TApp",    ["t", "args"])
-declare_case(Type, "TEnum",   ["cases"])
-declare_case(Type, "TTuple",  ["ts"])
+TInt                = declare_case(Type, "TInt")
+TLong               = declare_case(Type, "TLong")
+TBool               = declare_case(Type, "TBool")
+THandle             = declare_case(Type, "THandle", ["t"])
+TBag                = declare_case(Type, "TBag",    ["t"])
+TList               = declare_case(Type, "TList",   ["t"])
+TSet                = declare_case(Type, "TSet",    ["t"])
+TMap                = declare_case(Type, "TMap",    ["k", "v"])
+TNamed              = declare_case(Type, "TNamed",  ["id"])
+TRecord             = declare_case(Type, "TRecord", ["fields"])
+TApp                = declare_case(Type, "TApp",    ["t", "args"])
+TEnum               = declare_case(Type, "TEnum",   ["cases"])
+TTuple              = declare_case(Type, "TTuple",  ["ts"])
 
 class Exp(ADT): pass
-declare_case(Exp, "EVar",               ["id"])
-declare_case(Exp, "EBool",              ["val"])
-declare_case(Exp, "ENum",               ["val"])
-declare_case(Exp, "EBinOp",             ["e1", "op", "e2"])
-declare_case(Exp, "EUnaryOp",           ["op", "e"])
-declare_case(Exp, "EGetField",          ["e", "f"])
-declare_case(Exp, "EMakeRecord",        ["fields"])
-declare_case(Exp, "EListComprehension", ["e", "clauses"])
-declare_case(Exp, "EAlloc",             ["t", "args"])
-declare_case(Exp, "ECall",              ["func", "args"])
-declare_case(Exp, "ETuple",             ["es"])
+EVar                = declare_case(Exp, "EVar",               ["id"])
+EBool               = declare_case(Exp, "EBool",              ["val"])
+ENum                = declare_case(Exp, "ENum",               ["val"])
+EBinOp              = declare_case(Exp, "EBinOp",             ["e1", "op", "e2"])
+EUnaryOp            = declare_case(Exp, "EUnaryOp",           ["op", "e"])
+EGetField           = declare_case(Exp, "EGetField",          ["e", "f"])
+EMakeRecord         = declare_case(Exp, "EMakeRecord",        ["fields"])
+EListComprehension  = declare_case(Exp, "EListComprehension", ["e", "clauses"])
+EAlloc              = declare_case(Exp, "EAlloc",             ["t", "args"])
+ECall               = declare_case(Exp, "ECall",              ["func", "args"])
+ETuple              = declare_case(Exp, "ETuple",             ["es"])
 
 class ComprehensionClause(ADT): pass
-declare_case(ComprehensionClause, "CPull", ["id", "e"])
-declare_case(ComprehensionClause, "CCond", ["e"])
+CPull               = declare_case(ComprehensionClause, "CPull", ["id", "e"])
+CCond               = declare_case(ComprehensionClause, "CCond", ["e"])
 
 class Stm(ADT): pass
-declare_case(Stm, "SNoOp")
-declare_case(Stm, "SCall",   ["target", "func", "args"])
-declare_case(Stm, "SAssign", ["lhs", "rhs"])
-declare_case(Stm, "SDel",    ["e"])
+SNoOp               = declare_case(Stm, "SNoOp")
+SCall               = declare_case(Stm, "SCall",   ["target", "func", "args"])
+SAssign             = declare_case(Stm, "SAssign", ["lhs", "rhs"])
+SDel                = declare_case(Stm, "SDel",    ["e"])
