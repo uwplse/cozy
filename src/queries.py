375af4c01d5e0828e45ad5ee5ee4b492c3aea4c1
Calvin Loncaric <loncaric@cs.washington.edu>
2016-09-23 14:19:12 -0700

    Removed two more unused files

--- src/queries.py
+++
@@ -1,8 +0,0 @@
-
-class Query(object):
-    def __init__(self, name, vars, pred, assumptions=(), sort_field=None):
-        self.name = name
-        self.vars = vars
-        self.pred = pred
-        self.assumptions = assumptions
-        self.sort_field = sort_field
