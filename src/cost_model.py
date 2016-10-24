b99ac851cbd8316fb8ad00db693eae2550893a65
Calvin Loncaric <loncaric@cs.washington.edu>
2016-10-24 11:54:22 -0700

    Greatly improved cardinality estimation
    This helps our cost model substantially, and improves synthesis times by
    converging faster on good plans.

--- src/cost_model.py
+++ src/cost_model.py
@@ -8,26 +8,33 @@ import predicates
 from codegen_java import JavaCodeGenerator
 from codegen_cpp import CppCodeGenerator
 
+def _reduction(predicate):
+    """Estimate of the factor by which the predicate reduces the space of entries"""
+    f = 1.0
+    for p in predicates.break_conj(predicate):
+        if isinstance(p, predicates.Bool):
+            if p.val:
+                continue
+            else:
+                return 0
+        elif isinstance(p, predicates.Compare) and p.op == predicates.Eq:
+            f *= 0.2
+        else:
+            f *= 0.5
+    return f
+
 def _cost(plan, n=float(1000)):
     """Returns (cost,size) tuples"""
-    if isinstance(plan, plans.AllWhere): return 1, (n if plan.predicate == predicates.Bool(True) else (n / 2))
+    if isinstance(plan, plans.AllWhere): return 1, n * _reduction(plan.predicate)
     if isinstance(plan, plans.HashLookup):
         cost1, size1 = _cost(plan.plan)
-        return cost1 + 1, size1 / 3
+        return cost1 + 1, size1 * _reduction(plan.predicate)
     if isinstance(plan, plans.BinarySearch):
         cost1, size1 = _cost(plan.plan)
-        return cost1 + (math.log(size1) if size1 >= 1 else 1) + 1, size1 / 2
+        return cost1 + (math.log(size1) if size1 >= 1 else 1) + 1, size1 * _reduction(plan.predicate)
     if isinstance(plan, plans.Filter):
         cost1, size1 = _cost(plan.plan)
-        return cost1 + size1 + 1, size1 / 2
-    if isinstance(plan, plans.Intersect):
-        cost1, size1 = _cost(plan.plan1)
-        cost2, size2 = _cost(plan.plan2)
-        return cost1 + cost2 + size1 + size2 + 1, min(size1, size2) / 2
-    if isinstance(plan, plans.Union):
-        cost1, size1 = _cost(plan.plan1)
-        cost2, size2 = _cost(plan.plan2)
-        return cost1 + cost2 + size1 + size2 + 1, size1 + size2
+        return cost1 + size1 + 1, size1 * _reduction(plan.predicate)
     if isinstance(plan, plans.Concat):
         cost1, size1 = _cost(plan.plan1)
         cost2, size2 = _cost(plan.plan2)
