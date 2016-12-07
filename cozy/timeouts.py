e5b7f5df2dfb0846c63c7a64bd84264fff552a2b
Calvin Loncaric <loncaric@cs.washington.edu>
2016-12-06 17:42:03 -0800

    Added support for infinite timeouts

--- cozy/timeouts.py
+++ cozy/timeouts.py
@@ -6,9 +6,9 @@ class TimeoutException(Exception):
 class Timeout(object):
     def __init__(self, duration):
         self.duration = duration
-        self.expiration = datetime.datetime.now() + duration
+        self.expiration = datetime.datetime.now() + duration if duration is not None else None
     def is_timed_out(self):
-        return datetime.datetime.now() > self.expiration
+        return self.expiration is not None and datetime.datetime.now() > self.expiration
     def check(self):
         if self.is_timed_out():
             raise TimeoutException()
