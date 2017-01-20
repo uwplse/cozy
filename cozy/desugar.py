b039e5be4f135bbda648c8b587b23bd9902a5f7d
Calvin Loncaric <loncaric@cs.washington.edu>
2017-01-20 10:45:31 -0800

    Improved desugaring procedure

--- cozy/desugar.py
+++ cozy/desugar.py
@@ -33,59 +33,8 @@ def _handleize(m : Method, statevar : EVar):
     return m
 
 @typechecked
-def desugar(spec : Spec) -> Spec:
-
-    # rewrite enums
-    repl = {
-        name : EEnumEntry(name).with_type(t)
-        for t in all_types(spec)
-        if isinstance(t, TEnum)
-        for name in t.cases }
-    spec = subst(spec, repl)
-
-    spec = Spec(
-        spec.name,
-        list(spec.types),
-        list(spec.extern_funcs),
-        list(spec.statevars),
-        list(spec.assumptions),
-        list(spec.methods))
-
-    # convert all collection types to sets of handles
-    for i in range(len(spec.statevars)):
-        v, t = spec.statevars[i]
-        v = EVar(v).with_type(t)
-        if isinstance(t, TSet):
-            t = TBag(t.t)
-            spec.statevars[i] = (v.id, t)
-            v = EVar(v.id).with_type(t)
-            spec.assumptions.append(EUnaryOp("unique", v).with_type(BOOL))
-            assert retypecheck(spec, env=())
-        if isinstance(t, TBag):
-            if not isinstance(t.t, THandle) or not valid(implies(EAll(spec.assumptions), EUnaryOp("unique", v).with_type(BOOL))):
-                orig_t = t
-                ht = THandle(fresh_name("HandleType"), t.t)
-                t = TBag(ht)
-                v = EVar(v.id).with_type(t)
-                spec.statevars[i] = (v.id, t)
-                spec.assumptions = [_handleize(a, v) for a in spec.assumptions]
-                spec.assumptions.append(EUnaryOp("unique", v).with_type(BOOL))
-                spec.methods = [_handleize(m, v) for m in spec.methods]
-        else:
-            raise NotImplementedError(t)
-
-    assert retypecheck(spec, env=())
-
-    # organize queries by name
-    queries = { q.name : q for q in spec.methods if isinstance(q, Query) }
-
+def desugar_exp(e : Exp) -> Exp:
     class V(BottomUpRewriter):
-        def visit_ECall(self, e):
-            q = queries.get(e.func)
-            if q is not None:
-                return self.visit(subst(q.ret, { arg_name: arg for ((arg_name, ty), arg) in zip(q.args, e.args) }))
-            else:
-                return ECall(e.func, tuple(self.visit(a) for a in e.args)).with_type(e.type)
         def visit_EMap(self, e):
             bag = self.visit(e.e)
             fbody = self.visit(e.f.body)
@@ -177,15 +126,74 @@ def desugar(spec : Spec) -> Spec:
                 return EBinOp(e1, op, e2).with_type(e.type)
         def visit_EFlatMap(self, e):
             return EFlatten(EMap(e.e, e.f))
+    return V().visit(e)
 
-    e = V().visit(spec)
-    assert retypecheck(e, env={})
+@typechecked
+def desugar(spec : Spec) -> Spec:
 
-    for ee in all_exps(e):
+    # rewrite enums
+    repl = {
+        name : EEnumEntry(name).with_type(t)
+        for t in all_types(spec)
+        if isinstance(t, TEnum)
+        for name in t.cases }
+    spec = subst(spec, repl)
+
+    # convert all collection types to bags of handles
+    spec = Spec(
+        spec.name,
+        list(spec.types),
+        list(spec.extern_funcs),
+        list(spec.statevars),
+        list(spec.assumptions),
+        list(spec.methods))
+    for i in range(len(spec.statevars)):
+        v, t = spec.statevars[i]
+        v = EVar(v).with_type(t)
+        if isinstance(t, TSet):
+            t = TBag(t.t)
+            spec.statevars[i] = (v.id, t)
+            v = EVar(v.id).with_type(t)
+            spec.assumptions.append(EUnaryOp("unique", v).with_type(BOOL))
+            assert retypecheck(spec, env=())
+        if isinstance(t, TBag):
+            if not isinstance(t.t, THandle) or not valid(desugar_exp(implies(EAll(spec.assumptions), EUnaryOp("unique", v).with_type(BOOL)))):
+                orig_t = t
+                ht = THandle(fresh_name("HandleType"), t.t)
+                t = TBag(ht)
+                v = EVar(v.id).with_type(t)
+                spec.statevars[i] = (v.id, t)
+                spec.assumptions = [_handleize(a, v) for a in spec.assumptions]
+                spec.assumptions.append(EUnaryOp("unique", v).with_type(BOOL))
+                spec.methods = [_handleize(m, v) for m in spec.methods]
+        else:
+            raise NotImplementedError(t)
+
+    assert retypecheck(spec, env=())
+
+    # organize queries by name
+    queries = { q.name : q for q in spec.methods if isinstance(q, Query) }
+
+    class V(BottomUpRewriter):
+        def visit_ECall(self, e):
+            q = queries.get(e.func)
+            if q is not None:
+                return self.visit(subst(q.ret, { arg_name: arg for ((arg_name, ty), arg) in zip(q.args, e.args) }))
+            else:
+                return ECall(e.func, tuple(self.visit(a) for a in e.args)).with_type(e.type)
+    spec = V().visit(spec)
+
+    class V(BottomUpRewriter):
+        def visit_Exp(self, e):
+            return desugar_exp(e)
+    spec = V().visit(spec)
+
+    assert retypecheck(spec, env={})
+    for ee in all_exps(spec):
         if isinstance(ee, ELambda):
             if not isinstance(ee.arg.type, THandle):
                 import pdb
                 pdb.set_trace()
                 assert False
 
-    return e
+    return spec
