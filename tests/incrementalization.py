2c6cec01b2e18923cdbbc718ee1ec4387ca03df8
Calvin Loncaric <loncaric@cs.washington.edu>
2017-01-26 13:52:32 -0800

    Made a test more robust
    Instead of testing the exact return value from incrementalization, this test
    instead now checks the thing we want: that the returned derivative behaves
    correctly.

--- tests/incrementalization.py
+++ tests/incrementalization.py
@@ -2,17 +2,23 @@ import unittest
 
 from cozy import incrementalization as inc
 from cozy.target_syntax import *
+from cozy.syntax_tools import equal, pprint
+from cozy.solver import valid
 
 class TestDerivative(unittest.TestCase):
 
     def test_tuple_deriv(self):
-        x = EVar("x")
-        y = EVar("y")
-        e = ETuple((x, y))
-        delta = inc.AddNum(ENum(1))
+        x = EVar("x").with_type(INT)
+        y = EVar("y").with_type(INT)
+        tup = TTuple((INT, INT))
+        e = ETuple((x, y)).with_type(tup)
+        delta = inc.AddNum(ENum(1).with_type(INT))
         deriv, subgoals = inc.derivative(e, x, delta, [])
+        print("derivative = {}".format(pprint(deriv)))
         assert not subgoals
-        assert deriv == inc.TupleEntryUpdate(0, delta), "got {}".format(deriv)
+        applied = inc.apply_delta(e, deriv)
+        print("applied = {}".format(pprint(applied)))
+        assert valid(equal(applied, ETuple((EBinOp(x, "+", ENum(1).with_type(INT)).with_type(INT), y)).with_type(tup)))
 
 class TestApplyDelta(unittest.TestCase):
 
