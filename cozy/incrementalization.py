5dd03c75051346ddba7063c87b6b0780e6e21042
Calvin Loncaric <loncaric@cs.washington.edu>
2017-02-01 12:01:43 -0800

    Turn a generator expression into a proper tuple

--- cozy/incrementalization.py
+++ cozy/incrementalization.py
@@ -254,9 +254,9 @@ def apply_delta(
                 return target_syntax.EWithAlteredValue(x, apply_delta(syntax.EGetField(x, "val").with_type(x.type.value_type), delta.delta)).with_type(x.type)
             else:
                 assert isinstance(x.type, syntax.TRecord)
-                return syntax.EMakeRecord(
+                return syntax.EMakeRecord(tuple(
                     (f, syntax.EGetField(x, f).with_type(t) if f != delta.f else apply_delta(syntax.EGetField(x, f).with_type(t), delta.delta))
-                    for (f, t) in x.type.fields).with_type(x.type)
+                    for (f, t) in x.type.fields)).with_type(x.type)
         def visit_TupleEntryUpdate(self, delta):
             if isinstance(x, syntax.ETuple):
                 return syntax.ETuple(tuple(
