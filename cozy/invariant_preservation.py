43097b80fa4ea362dc173f736527a0d9c8861f15
Calvin Loncaric <loncaric@cs.washington.edu>
2017-04-19 14:13:28 -0700

    Added an internal option to skip invariant preservation checks

--- cozy/invariant_preservation.py
+++ cozy/invariant_preservation.py
@@ -2,8 +2,13 @@ from cozy.syntax import *
 from cozy.solver import valid
 from cozy.syntax_tools import pprint
 from cozy.incrementalization import to_delta, derivative, apply_delta
+from cozy.opts import Option
+
+invariant_preservation_check = Option("invariant-preservation-check", bool, True)
 
 def check_ops_preserve_invariants(spec : Spec):
+    if not invariant_preservation_check.value:
+        return []
     res = []
     for m in spec.methods:
         if not isinstance(m, Op):
