a6f7a41b4b9d25e205a6c515c4b478d730e74105
Calvin Loncaric <loncaric@cs.washington.edu>
2017-10-12 14:45:39 -0700

    Handle tuples and records in construct_concrete

--- cozy/codegen/cxx.py
+++ cozy/codegen/cxx.py
@@ -199,7 +199,7 @@ class CxxPrinter(common.Visitor):
             return SSeq(
                 self.initialize_native_map(out),
                 self.construct_map(t, e, out))
-        elif type(t) in [TBool, TInt, TNative, THandle, TLong, TString, TEnum]:
+        elif type(t) in [TBool, TInt, TNative, THandle, TLong, TString, TEnum, TTuple, TRecord]:
             return SEscape("{indent}{lhs} = {rhs};\n", ["lhs", "rhs"], [out, e])
         raise NotImplementedError(t)
 
