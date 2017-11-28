31266556f2c0ba685ed95c24a8a027f8e0358e6d
Calvin Loncaric <loncaric@cs.washington.edu>
2017-11-27 18:22:57 -0800

    Allow integer-type options

--- cozy/opts.py
+++ cozy/opts.py
@@ -2,7 +2,7 @@ _OPTS = []
 
 class Option(object):
     def __init__(self, name, type, default, description="", metavar=None):
-        assert type in (bool, str)
+        assert type in (bool, str, int)
         self.name = name
         self.description = description
         self.type = type
@@ -21,7 +21,7 @@ def setup(parser):
         n = _argname(o)
         if o.type is bool:
             parser.add_argument("--" + n, action="store_true", default=False, help=o.description)
-        elif o.type is str:
+        elif o.type in (str, int):
             parser.add_argument("--" + n, metavar=o.metavar, default=o.default, help=(o.description + " (default={})".format(repr(o.default))) if o.description else "default={}".format(repr(o.default)))
 
 def read(args):
@@ -29,3 +29,5 @@ def read(args):
         o.value = getattr(args, _argname(o).replace("-", "_"))
         if o.type is bool and o.default:
             o.value = not o.value
+        if o.type is int:
+            o.value = int(o.value)
