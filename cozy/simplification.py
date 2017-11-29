e4f1a2972206be7a0762ed41d7baa887fa70d187
Calvin Loncaric <loncaric@cs.washington.edu>
2017-11-28 16:19:42 -0800

    Much smarter simplification logic

--- cozy/simplification.py
+++ cozy/simplification.py
@@ -1,8 +1,11 @@
 from cozy.target_syntax import *
-from cozy.syntax_tools import BottomUpRewriter, alpha_equivalent
+from cozy.syntax_tools import BottomUpRewriter, alpha_equivalent, cse
 from cozy.evaluation import construct_value
+from cozy.solver import valid
 
 class _V(BottomUpRewriter):
+    def __init__(self, debug=False):
+        self.debug = debug
     def visit_EBinOp(self, e):
         if e.op == BOp.In:
             if isinstance(e.e2, EBinOp) and e.e2.op == "+":
@@ -12,8 +15,22 @@ class _V(BottomUpRewriter):
             elif isinstance(e.e2, EFilter):
                 return self.visit(EAll([EIn(e.e1, e.e2.e), e.e2.p.apply_to(e.e1)]))
         elif e.op in ("==", "==="):
-            if alpha_equivalent(self.visit(e.e1), self.visit(e.e2)):
+            e1 = self.visit(e.e1)
+            e2 = self.visit(e.e2)
+            if alpha_equivalent(e1, e2):
                 return T
+            if e.op == "==":
+                while isinstance(e1, EWithAlteredValue): e1 = e1.handle
+                while isinstance(e2, EWithAlteredValue): e2 = e2.handle
+            e = EBinOp(e1, e.op, e2).with_type(e.type)
+        if isinstance(e.e1, ECond):
+            return self.visit(ECond(e.e1.cond,
+                EBinOp(e.e1.then_branch, e.op, e.e2).with_type(e.type),
+                EBinOp(e.e1.else_branch, e.op, e.e2).with_type(e.type)).with_type(e.type))
+        if isinstance(e.e2, ECond):
+            return self.visit(ECond(e.e2.cond,
+                EBinOp(e.e1, e.op, e.e2.then_branch).with_type(e.type),
+                EBinOp(e.e1, e.op, e.e2.else_branch).with_type(e.type)).with_type(e.type))
         return EBinOp(self.visit(e.e1), e.op, self.visit(e.e2)).with_type(e.type)
     def visit_ECond(self, e):
         cond = self.visit(e.cond)
@@ -30,5 +47,42 @@ class _V(BottomUpRewriter):
                 e.map.value.apply_to(e.key),
                 construct_value(e.type)).with_type(e.type))
         return EMapGet(self.visit(e.map), self.visit(e.key)).with_type(e.type)
+    def visit_EWithAlteredValue(self, e):
+        t = e.type
+        addr = self.visit(e.handle)
+        val = self.visit(e.new_value)
+        while isinstance(addr, EWithAlteredValue): addr = addr.handle
+        if isinstance(addr, ECond):
+            return self.visit(ECond(addr.cond,
+                EWithAlteredValue(addr.then_branch, val).with_type(t),
+                EWithAlteredValue(addr.else_branch, val).with_type(t)).with_type(t))
+        return EWithAlteredValue(addr, val).with_type(t)
+    def visit_EGetField(self, e):
+        record = self.visit(e.e)
+        if isinstance(record, ECond):
+            return self.visit(ECond(record.cond,
+                EGetField(record.then_branch, e.f).with_type(e.type),
+                EGetField(record.else_branch, e.f).with_type(e.type)).with_type(e.type))
+        if isinstance(record, EWithAlteredValue) and e.f == "val":
+            return record.new_value
+        if isinstance(record, EMakeRecord):
+            return dict(record.fields)[e.f]
+        return EGetField(record, e.f).with_type(e.type)
+    def visit(self, e):
+        new = super().visit(e)
+        if self.debug and isinstance(e, Exp) and not isinstance(e, ELambda):
+            if not valid(EBinOp(e, "===", new).with_type(BOOL)):
+                from cozy.syntax_tools import pprint
+                raise Exception("bad simplification: {} ---> {}".format(pprint(e), pprint(new)))
+        return new
 
-simplify = _V().visit
+def simplify(e, debug=False):
+    visitor = _V(debug)
+    orig = e
+    e = visitor.visit(e)
+    # e = cse(e)
+    if not valid(EBinOp(orig, "===", e).with_type(BOOL)):
+        import sys
+        print("simplify did something stupid!\nto reproduce:\nsimplify({e!r}, debug=True)".format(e=orig), file=sys.stderr)
+        return orig
+    return e
