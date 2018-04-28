273719e73d7fbeaa9f1a3dade5c197b84f2ac575
Calvin Loncaric <loncaric@cs.washington.edu>
2018-04-27 18:44:11 -0700

    Added another map acceleration regression test

--- tests/acceleration.py
+++ tests/acceleration.py
@@ -3,7 +3,7 @@ import unittest
 from cozy.target_syntax import *
 from cozy.syntax_tools import pprint, alpha_equivalent, free_vars
 from cozy.typecheck import retypecheck
-from cozy.contexts import RootCtx
+from cozy.contexts import RootCtx, UnderBinder
 from cozy.pools import RUNTIME_POOL
 from cozy.synthesis.acceleration import map_accelerate
 
@@ -25,3 +25,14 @@ class TestAccelerationRules(unittest.TestCase):
 
         expected = EMapGet(EStateVar(EMakeMap2(xs, ELambda(y, EFilter(xs, ELambda(x, EEq(x, y)))))), ONE)
         assert any(alpha_equivalent(ee, expected) for ee in results), "results = {}".format("; ".join(pprint(ee) for ee in results))
+
+    def test_map_accel_regression2(self):
+        x = EVar("x").with_type(INT)
+        y = EVar("y").with_type(INT)
+        pairs = EVar("pairs").with_type(TBag(TTuple((INT, INT))))
+        xs = EVar("xs").with_type(INT_BAG)
+        ctx = RootCtx(state_vars=[pairs, xs], args=[x])
+        ctx = UnderBinder(ctx, y, EStateVar(xs).with_type(xs.type), RUNTIME_POOL)
+        e = EIn(ETuple((x, y)), pairs)
+        assert retypecheck(e)
+        assert [res for res, pool in map_accelerate(e, ctx) if pool == RUNTIME_POOL and isinstance(res, EMapGet)]
