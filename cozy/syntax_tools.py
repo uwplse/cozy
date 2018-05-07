uwplse/cozy

commit 343acbc82a18654a6df4ad549e6c679406afac9e

Allow `all_exps` to work on huge expressions

cozy/syntax_tools.py
@@ -579,14 +579,17 @@ def free_vars(exp, counts=False):
         res = common.OrderedSet(res.keys())
     return res
 
-def all_exps(e):
-    class V(BottomUpExplorer):
-        def join(self, x, children):
-            for child in children:
-                yield from child
-            if isinstance(x, syntax.Exp):
-                yield x
-    return V().visit(e)
+def all_exps(e : syntax.Exp):
+    q = [e]
+    while q:
+        e = q.pop()
+        if isinstance(e, tuple) or isinstance(e, list):
+            q.extend(e)
+            continue
+        if not isinstance(e, syntax.Exp):
+            continue
+        yield e
+        q.extend(e.children())
 
 Unknown = collections.namedtuple("Unknown", [])
 ElemOf = collections.namedtuple("ElemOf", ["bag"])

tests/syntax_tools.py
@@ -253,6 +253,14 @@ class TestSyntaxTools(unittest.TestCase):
         assert l[2] is y
         assert l[3] is z
 
+    def test_all_exps_recursion_depth(self):
+        n = 100000
+        e = ENum(-1).with_type(INT)
+        for i in range(n):
+            e = EBinOp(e, "+", ENum(i).with_type(INT))
+        for x in all_exps(e):
+            pass
+
     def test_get_modified_var(self):
         htype = THandle("IntPtr", INT)
 

commit 0a8d4b1220304de7dea03c541d6540f0168ee0fd

Fixed a bug where all_exps did not work on statements
The bug was introduced in commit 343acbc8.

cozy/syntax_tools.py
@@ -579,17 +579,17 @@ def free_vars(exp, counts=False):
         res = common.OrderedSet(res.keys())
     return res
 
-def all_exps(e : syntax.Exp):
-    q = [e]
+def all_exps(x):
+    q = [x]
     while q:
         e = q.pop()
         if isinstance(e, tuple) or isinstance(e, list):
             q.extend(e)
             continue
-        if not isinstance(e, syntax.Exp):
-            continue
-        yield e
-        q.extend(e.children())
+        if isinstance(e, syntax.Exp):
+            yield e
+        if isinstance(e, common.ADT):
+            q.extend(e.children())
 
 Unknown = collections.namedtuple("Unknown", [])
 ElemOf = collections.namedtuple("ElemOf", ["bag"])

tests/syntax_tools.py
@@ -261,6 +261,10 @@ class TestSyntaxTools(unittest.TestCase):
         for x in all_exps(e):
             pass
 
+    def test_all_exps_on_stm(self):
+        s = SIf(T, SNoOp(), SNoOp())
+        assert list(all_exps(s)) == [T]
+
     def test_get_modified_var(self):
         htype = THandle("IntPtr", INT)
 