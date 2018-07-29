e3468039f8794481d483dc351880faab190bedfe
Corbin <cds@corbinsimpson.com>
2018-07-28 21:57:32 -0700

    codegen/rpython: Complete handling of the examples.

--- cozy/codegen/rpython.py
+++ cozy/codegen/rpython.py
@@ -8,7 +8,7 @@ from contextlib import contextmanager
 from collections import defaultdict
 
 from cozy.codegen.misc import CodeGenerator
-from cozy.target_syntax import EEmptyList, ESingleton
+from cozy.target_syntax import EBool, EEmptyList, ESingleton
 
 class RPythonPrinter(CodeGenerator):
 
@@ -16,6 +16,7 @@ class RPythonPrinter(CodeGenerator):
         super(RPythonPrinter, self).__init__(*args, **kwargs)
 
         self.needs = defaultdict(bool)
+        self.instance_names = set()
 
     @contextmanager
     def python_block(self, *args):
@@ -56,7 +57,11 @@ class RPythonPrinter(CodeGenerator):
         self.write("]")
 
     def visit_EVar(self, node):
-        self.write(node.id)
+        name = node.id
+        if name in self.instance_names:
+            self.write("self.", name)
+        else:
+            self.write(name)
 
     def emit_unary(self, op, e):
         self.needs[op] = True
@@ -68,6 +73,10 @@ class RPythonPrinter(CodeGenerator):
         op = node.op
         if op in ("distinct", "sum"):
             self.emit_unary(op, node.e)
+        elif op == "len":
+            self.write("len(")
+            self.visit(node.e)
+            self.write(")")
         elif op == "not":
             self.write("not ")
             self.visit(node.e)
@@ -81,7 +90,7 @@ class RPythonPrinter(CodeGenerator):
 
     def visit_EBinOp(self, node):
         op = node.op
-        if op in (">", "<", "+", "-", "==", "in", "and", "or"):
+        if op in (">", "<", "+", "-", "==", "!=", "in", "and", "or"):
             with self.parens():
                 self.visit(node.e1)
                 self.write(" ", op, " ")
@@ -91,14 +100,25 @@ class RPythonPrinter(CodeGenerator):
             assert False, "Unhandled binary operation while writing RPython"
 
     def visit_ECond(self, node):
-        self.visit(node.then_branch)
-        self.write(" if ")
-        self.visit(node.cond)
-        self.write(" else ")
-        self.visit(node.else_branch)
+        # Quirk: Sometimes cond will be constantly true or false.
+        if isinstance(node, EBool):
+            self.visit(node.then_branch if node.val else node.else_branch)
+
+        with self.parens():
+            self.visit(node.then_branch)
+            self.write(" if ")
+            self.visit(node.cond)
+            self.write(" else ")
+            self.visit(node.else_branch)
 
     def visit_EMap(self, node):
         f = node.f
+        # Quirk: Sometimes we will see (map (\x -> x) xs) and we want to just
+        # have xs instead.
+        if f.arg == f.body:
+            self.visit(node.e)
+            return
+
         self.write("[")
         self.visit(f.body)
         self.write(" for ")
@@ -121,6 +141,28 @@ class RPythonPrinter(CodeGenerator):
         self.write(" if ")
         self.visit(p.body)
         self.write("]")
+
+    def visit_EArgMax(self, node):
+        f = node.f
+        self.needs["argMax"] = True
+        self.write("_argMax([")
+        self.visit(f.body)
+        self.write(" for ")
+        self.visit(f.arg)
+        self.write(" in ")
+        self.visit(node.e)
+        self.write("])")
+
+    def visit_EArgMin(self, node):
+        f = node.f
+        self.needs["argMin"] = True
+        self.write("_argMin([")
+        self.visit(f.body)
+        self.write(" for ")
+        self.visit(f.arg)
+        self.write(" in ")
+        self.visit(node.e)
+        self.write("])")
 
     def visit_EMakeMap2(self, node):
         self.needs["makeMap"] = True
@@ -159,9 +201,7 @@ class RPythonPrinter(CodeGenerator):
             return
 
         self.visit(node.e)
-        self.write("[")
-        self.visit(node.n)
-        self.write("]")
+        self.write("[", str(node.n), "]")
 
     def visit_EGetField(self, node):
         self.visit(node.e)
@@ -275,6 +315,7 @@ class RPythonPrinter(CodeGenerator):
             init_args = ", ".join([name for name, _ in abstract_state])
             with self.python_block("def __init__(self, ", init_args, ")"):
                 for name, exp in state_exps.items():
+                    self.instance_names.add(name)
                     with self.stmt():
                         self.write("self.", name, " = ")
                         self.visit(exp)
@@ -288,6 +329,26 @@ class RPythonPrinter(CodeGenerator):
             # Suddenly, globals()! These helpers are textually defined at the
             # end of the module. ~ C.
             self.write(globals()[op.upper()])
+
+ARGMAX = """
+@specialize.call_location()
+def _argMax(it):
+    assert len(it)
+    x = it[0]
+    for y in it:
+        x = max(x, y)
+    return x
+"""
+
+ARGMIN = """
+@specialize.call_location()
+def _argMin(it):
+    assert len(it)
+    x = it[0]
+    for y in it:
+        x = min(x, y)
+    return x
+"""
 
 DISTINCT = """
 @specialize.call_location()
