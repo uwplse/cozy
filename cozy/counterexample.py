00646ad5e3db6a2a6cd68f73b2859443a6ee3de3
Zhen Zhang <izgzhen@gmail.com>
2019-10-12 15:52:45 -0700

    Use iterables

--- cozy/counterexample.py
+++ cozy/counterexample.py
@@ -1,5 +1,6 @@
 import random
 import string
+from itertools import product
 
 from cozy.common import FrozenDict
 from cozy.syntax import TBag, TInt, TString, ENum, EStr, TRecord, ESingleton, EMakeRecord
@@ -10,17 +11,20 @@ from cozy.evaluation import eval, uneval
 # FIXME: how to use solver for this purpose?
 from cozy.value_types import Bag
 
-
-def construct_random_value(t):
+def random_value(t):
     if isinstance(t, TBag):
-        return Bag((construct_random_value(t.elem_type),))
-    if isinstance(t, TInt):
-        return random.randint(0, 100)
-    if isinstance(t, TString):
-        return ''.join(random.choice(string.ascii_letters) for _ in range(8))
-    if isinstance(t, TRecord):
-        return FrozenDict({ name : construct_random_value(ty) for name, ty in t.fields })
-    raise Exception("Unknown type for random value construction: {}".format(t))
+        for v in random_value(t.elem_type):
+            yield Bag((v,))
+    elif isinstance(t, TInt):
+        yield random.randint(0, 100)
+    elif isinstance(t, TString):
+        yield ''.join(random.choice(string.ascii_letters) for _ in range(8))
+    elif isinstance(t, TRecord):
+        iterables = [ random_value(ty) for _, ty in t.fields ]
+        for vs in product(*iterables):
+            yield FrozenDict({ field[0] : v for v, field in zip(vs, t.fields) })
+    else:
+        raise Exception("Unknown type for random value construction: {}".format(t))
 #
 # def reconstruct(e):
 #     if isinstance(e, ESingleton):
@@ -30,13 +34,16 @@ def construct_random_value(t):
 #     return e
 
 def find_counterexample(e):
-    assignments = {}
     assignments_reconstructed = {}
-    for v in free_vars(e):
-        val = construct_random_value(v.type)
-        assignments[v.id] = val
+    iterables = [ random_value(v.type) for v in free_vars(e) ]
+    ids = [ v.id for v in free_vars(e) ]
+    for vs in product(*iterables):
+        assignments = {}
+        for id, val in zip(ids, vs):
+            assignments[id] = val
         # assignments_reconstructed[v.id] = reconstruct(val)
-    sat = eval(e, assignments)
-    if not sat:
-        return assignments
+        print("[DEBUG]: assignments: %s" % assignments)
+        sat = eval(e, assignments)
+        if not sat:
+            return assignments
     return None
--- tests/counterexamples_driver.py
+++ tests/counterexamples_driver.py
@@ -4,4 +4,5 @@ from tests.counterexamples import *
 for target, new_target, counterexample in tests:
     e = ENot(EEq(target, new_target))
     counterexample_2 = find_counterexample(e)
-    assert counterexample == counterexample_2, pprint(e)
+    if counterexample_2 is None:
+        print("missed counter-example: %s\nfor: %s\n" % (counterexample, pprint(e)))
