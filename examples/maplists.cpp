726e5081b050b211f3a5699423e070079aabf0a3
Zhen Zhang <izgzhen@gmail.com>
2019-10-24 10:55:34 -0700

    Add a slow example

---
+++ examples/maplists.cpp
@@ -0,0 +1,31 @@
+// listcomp example main driver
+#include <iostream>
+
+#include "maplists.h"
+
+#include <chrono>
+
+using namespace std::chrono;
+
+constexpr int max_iter = 10000;
+
+long now_ms() {
+    milliseconds ms = duration_cast< milliseconds >(
+        system_clock::now().time_since_epoch()
+    );
+    return ms.count();
+}
+
+int main(int argc, char const *argv[])
+{
+    MapOfLists l;
+    long initial_count = now_ms();
+    for (int i = 0; i < max_iter; i++) {
+        if (i % (max_iter / 100) == 0) {
+            std::cout << i << " " << now_ms() - initial_count << std::endl;
+        }
+        l.insert_r(MapOfLists::R(1, ""));
+        l.insert_s(MapOfLists::S("a", 2));
+    }
+    return 0;
+}
